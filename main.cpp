28f934df9c338ab4895cd26283babbe2087fa343
Szymon Gajewski <szymongaju@gmail.com>
2019-07-09 17:08:02 +0200

    Palindrome function

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+string sentence;
+
+bool palindrome(string sentence)
+{
+    int x=0;
+    sentence.erase(remove(sentence.begin(), sentence.end(), ' '), sentence.end());
+    int length=sentence.length();
+        if(length==0){cout<<"No sentence was given.";exit(0);}
+            for(int i=0; i<(length/2) ;i++)
+            {
+                if(sentence[x]==sentence[length-x-1])
+                x++;
+            }
+        if (x==length/2) return true;
+        else return false;
+}
+
+int main()
+{
+    cout<<"Write a sentence to check: ";
+    getline(cin, sentence);
+    if(palindrome(sentence)){
+    cout<<"It's a palindrome.";}
+    else{
+    cout<<"It isn't a palindrome.";}
+
+    return 0;
+}
